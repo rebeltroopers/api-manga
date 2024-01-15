a0aa13e7f3fc43c3d7e3347afbb31bea4e287367
rebeltrouper <rebeltrouper>
2024-01-15 12:51:14 -0800

    first commit creating a small template for kana

---
+++ README.txt
@@ -0,0 +1,5 @@
+Creating a api repository to test and learn how to use APIs.
+
+kana project.
+The point of the Kana project will be to get general information form visual
+novels.
---
+++ kana/kana.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+def api_get(filters = [], fields = "" ,sort="id", reverse = 'false',results = 10, page=1,user='null',count='false',compact_filters = 'false',normalized_filters='false'):
+    baseurl = 'https://api.vndb.org/kana/vn' 
+    sendjson = { 
+  "filters": filters, 
+  "fields": fields,
+  "sort": sort,
+#  "reverse": reverse,
+  "results": results,
+  "page": page,
+#  "user": 'null',
+#  "count": count,
+#  "compact_filters": compact_filters,
+#  "normalized_filters": normalized_filters
+}
+    print(sendjson)
+    response = requests.post(baseurl,json=sendjson)
+    print(response)
+    print(response.json())
+
+
+#for searching up tag names
+def search_loopup(searchitem = []):
+    baseurl = 'https://api.vndb.org/kana/tag' 
+
+
+
+    
+
+api_get(filters = ["search", "=",'cyberpunk'], fields = "id")
+
+#search_loopup()
+
+
